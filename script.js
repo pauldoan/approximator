bf31011794ce0f5c4be30f1248a413eb11d2c5aa
pauldoan <heypauldoan@gmail.com>
2023-01-22 11:21:20 -0500

    Adding calcultaor vs Approximator

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const title = document.querySelector("h1.title");
 const topDisplay = document.querySelector("#topValue");
 const bottomDisplay = document.querySelector("#bottomValue");
 const deleteBtn = document.querySelector("#delete");
@@ -19,9 +20,54 @@ const n7Btn = document.querySelector("#button7");
 const n8Btn = document.querySelector("#button8");
 const n9Btn = document.querySelector("#button9");
 const digitBtns = document.querySelectorAll(".digit,.operation");
+const modeBtn = document.querySelector("button.mode");
+let mode;
 const welcomeMessage = "Hi!";
 let topValue = "";
 let bottomValue = "";
+const operationMap = {
+  "+": addNumbers,
+  "-": subtractNumbers,
+  x: multiplyNumbers,
+  "÷": divideNumbers,
+};
+
+function funAnswer(result) {
+  const nAnswers = 5;
+  const randNb = Math.random() * nAnswers;
+  if (randNb < 1) {
+    return `I believe it's half of ${2 * result}`;
+  } else if (randNb < 2) {
+    return `Approximately a third of ${3 * result}`;
+  } else if (randNb < 3) {
+    if (result > 100) {
+      return `Wow, that's a big number!`;
+    } else {
+      return `It's below 100...`;
+    }
+  } else if (randNb < 4) {
+    return `It's between ${result - 1} and ${result + 1}`;
+  } else {
+    return `It's around ${Math.round(result)}`;
+  }
+}
+
+function changeMode() {
+  if (mode === "approximator") {
+    mode = "calculator";
+    title.textContent = "The Calculator";
+    equalBtn.textContent = "=";
+  } else {
+    mode = "approximator";
+    title.textContent = "The Approximator";
+    equalBtn.textContent = "≃";
+  }
+  clearScreen();
+  bottomValue = welcomeMessage;
+  displayScreen();
+}
+
+modeBtn.addEventListener("click", changeMode);
 
 function displayScreen() {
   topDisplay.textContent = topValue;
@@ -43,23 +89,119 @@ clearBtn.addEventListener("click", clearScreen);
 deleteBtn.addEventListener("click", deleteScreen);
 
 function inputButton() {
-  // reset the welcome message
+  // if still on welcome screen, we reset
   if (bottomValue === welcomeMessage) {
     bottomValue = "";
   }
-  // if we add digit
+  // if we add digit, we append to the bottom value
   if (this.className === "digit") {
+    // if we are in approximator and computation was just done, we auto first reset
+    if (
+      mode === "approximator" &&
+      /^-?\d+(\.\d+)? [+x\-÷] \d+(\.\d+)? ≃$/.test(topValue)
+    ) {
+      clearScreen();
+    }
     bottomValue += this.value;
   }
-  // if we add operation
-  else {
-    topValue = bottomValue + this.value;
-    bottomValue = "";
+  // if the first button pressed is a minus sign,
+  // we still append to digits for negative numbers
+  else if (
+    this.id === "subtract" &&
+    (bottomValue === "" ||
+      (mode === "approximator" &&
+        /^-?\d+(\.\d+)? [+x\-÷] \d+(\.\d+)? ≃$/.test(topValue)))
+  ) {
+    topValue = "";
+    bottomValue = this.value;
+  }
+  // if we add operation, we shift to top value and wait for next expression
+  else if (/^-?\d+(\.\d+)?$/.test(bottomValue)) {
+    // if we are in approximator and computation was just done, we throw error
+    if (
+      mode === "approximator" &&
+      /^-?\d+(\.\d+)? [+x\-÷] \d+(\.\d+)? ≃$/.test(topValue)
+    ) {
+      bottomValue = "input digit first!";
+    }
+    // if we are in approximator and we type a second operator
+    else if (
+      mode === "approximator" &&
+      /^-?\d+(\.\d+)? [+x\-÷]$/.test(topValue)
+    ) {
+      // we don't do anything
+    }
+
+    // if we are in calculator and we type a second operator
+    else if (
+      mode === "calculator" &&
+      /^-?\d+(\.\d+)? [+x\-÷]$/.test(topValue)
+    ) {
+      // we compute the result by simulating an equal
+      result = getResult();
+      // we append the sign to the result and move to next step
+      topValue = result[0] + " " + this.value;
+      bottomValue = "";
+    }
+
+    // any other case
+    else {
+      topValue = bottomValue + " " + this.value;
+      bottomValue = "";
+    }
   }
   displayScreen();
 }
 
+// calculator mode
+function getResult() {
+  // just equal on number
+  if (topValue === "" && /^-?\d+(\.\d+)?$/.test(bottomValue)) {
+    return [bottomValue, ""];
+  }
+  // if operation has been done and we repress equal, don't do anythin
+  else if (
+    /^-?\d+(\.\d+)? [+x\-÷] \d+(\.\d+)? ≃$/.test(topValue) &&
+    /^-?\d+(\.\d+)?$/.test(bottomValue)
+  ) {
+    return [bottomValue, topValue];
+  }
+  // one of the two expression has bad format
+  else if (
+    !/^-?\d+(\.\d+)? [+x\-÷]$/.test(topValue) ||
+    !/^\d+(\.\d+)?$/.test(bottomValue)
+  ) {
+    return ["Error :(", topValue];
+  }
+  // all good, we compute the operation
+  else {
+    const firstValue = Number(topValue.slice(0, -1));
+    const secondValue = Number(bottomValue);
+    const operation = topValue.slice(-1);
+    const operator = operationMap[operation];
+    const result = operate(operator, firstValue, secondValue);
+    const fullOperation = `${firstValue} ${operation} ${bottomValue} ${equalBtn.textContent}`;
+    return [result, fullOperation];
+  }
+}
+
 digitBtns.forEach((button) => button.addEventListener("click", inputButton));
+
+if (mode === "calculator") {
+  equalBtn.addEventListener("click", () => {
+    result = getResult();
+    bottomValue = approximate(result[0]);
+    topValue = result[1];
+    displayScreen();
+  });
+} else {
+  equalBtn.addEventListener("click", () => {
+    result = getResult();
+    bottomValue = funAnswer(result[0]);
+    topValue = result[1];
+    displayScreen();
+  });
+}
 
 function addNumbers(a, b) {
   return a + b;
@@ -80,8 +222,7 @@ function operate(operator, a, b) {
   return operator(a, b);
 }
 
-function getResult(expression) {}
-
 window.addEventListener("load", () => (bottomValue = welcomeMessage));
 window.addEventListener("load", () => (topValue = ""));
+window.addEventListener("load", () => (mode = "approximator"));
 window.addEventListener("load", displayScreen);
